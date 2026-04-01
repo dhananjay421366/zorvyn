fef90eba364026f4085583fbf041591bd89e5138
dhananjay421366 <nimbalkardhananjay349@gmail.com>
2026-04-01 18:46:21 +0530

    fx: build issue fix

--- utils/db.js
+++ utils/db.js
@@ -1,9 +1,6 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
-
-console.log("MONGO_URL:", process.env.MONGO_URL);
-
 export const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
